a76a11c19ac0415d03968f859bc7ce89c716a173
Lisa ^^ <islahola>
2023-03-13 20:16:33 +0700

    oke

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Rute from './rute';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../index.css'
 
 const Index = () => {
     return (
